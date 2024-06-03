2294beba2d9bf5bf7b349166db39080a9bbacbfa
Annaa19980530 <167744917+Annaa19980530@users.noreply.github.com>
2024-06-03 07:57:13 +0300

    Add files via upload

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    @Mock
+    Feline feline; //мок класса Feline
+
+    @Test //метод getSound() класса Cat возвращает "Мяу"
+    public void testGetSoundCat() {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test //метод eatMeat() вызван 2 раза
+    public void testGetFoodForCat() throws Exception {
+        Cat cat = new Cat(feline);
+        cat.getFood();
+        cat.getFood();
+        Mockito.verify(feline, Mockito.times(2)).eatMeat();
+    }
+
+    @Test //метод getFood возвращает список пищи кошки
+    public void catGetFoodPredatorList() throws Exception {
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+    }
+}
